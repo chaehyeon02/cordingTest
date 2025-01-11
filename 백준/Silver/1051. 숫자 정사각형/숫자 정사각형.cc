b0f84fe7a1dfa62e7975061ef6c7706bd329af7e
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-01-11 16:27:58 +0900

    [Silver III] Title: 숫자 정사각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    int n, m, size, ans = -1;
+    int square[50][50];
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < m; j++) {
+            square[i][j] = (int)s[j] - 48;
+        }
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m - 1; j++) {
+            for (int k = 1; k < n - i && k < m - j; k++) {
+                if (square[i][j] == square[i + k][j] && square[i][j] == square[i][j + k] && square[i][j] == square[i + k][j + k]) {
+                    if (ans < (k + 1) * (k + 1)) {
+                        ans = (k + 1) * (k + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    if (ans < 0) cout << "1";
+    else cout << ans;
+
+    return 0;
+}
