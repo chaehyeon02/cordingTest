8498fa7ccc59e89562782148b0eefbe391de6c5d
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-01-19 19:33:41 +0900

    [Silver I] Title: 스타트와 링크, Time: 76 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,52 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
-int n, minAns = 100;
+int n, minDiff = 100;
 int ability[20][20];
-bool team[20] = {};
+vector<int> teamA;
 
-void FindMin(int member, int cnt) {
-	vector<int> teamA;
-	vector<int> teamB;
-	int abilityA = 0, abilityB = 0;
-
-	//팀이 다 찬 경우
-	if (cnt == n / 2) {
-		for (int i = 0; i < n; i++) {
-			if (team[i]) teamA.push_back(i); //각 팀에 넣음
-			else teamB.push_back(i);
+void makeTeam(int member) {
+	teamA.push_back(member);
+	//팀원 n/2명
+	if (teamA.size() == n / 2) {
+		vector<int> teamB;
+		int abilityA = 0, abilityB = 0;
+		for (int i = 0; i < n; i++) { //teamB 만들기
+			if (find(teamA.begin(), teamA.end(), i) == teamA.end()) {
+				teamB.push_back(i);
+			}
 		}
+
 		for (int i = 0; i < n / 2; i++) {
 			for (int j = 0; j < n / 2; j++) {
 				abilityA += ability[teamA[i]][teamA[j]];
 				abilityB += ability[teamB[i]][teamB[j]];
 			}
 		}
-		if (abs(abilityA - abilityB) < minAns) minAns = abs(abilityA - abilityB);
+
+		if (abs(abilityA - abilityB) < minDiff) minDiff = abs(abilityA - abilityB);
 		return;
 	}
 
-	//팀원을 더 골라야 하는 경우
-	for (int i = member; i < n; i++) {
-		if (team[i]) continue;
-		else {
-			team[i] = true;
-			FindMin(i, cnt + 1);
-			team[i] = false;
-		}
+	//팀원 더 모으기
+	for (int i = member + 1; i < n; i++) {
+		makeTeam(i);
+		teamA.pop_back();
 	}
+	return;
 }
 
 int main() {
-
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) cin >> ability[i][j];
 	}
-	FindMin(0, 0);
-	cout << minAns;
+
+	for (int i = 0; i < n; i++) {
+		makeTeam(i);
+		teamA.pop_back();
+	}
+	cout << minDiff;
 
 	return 0;
 }
