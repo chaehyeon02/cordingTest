65bd80cb458d5930439f1a42f9ee99c601196ac5
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-01-24 21:31:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 20 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void bfs(int n, int k) {
+	queue<pair<int, int>> q;
+	bool visitNum[100001] = { false };
+
+	q.push(make_pair(n, 0));
+	visitNum[n] = true;
+	while (!q.empty()) {
+		/*if ((q.front().first < 0 || q.front().first > 100000) && visitNum[q.front().first]) {
+			q.pop();
+			continue;
+		}*/
+
+		if (q.front().first == k) {
+			cout << q.front().second;
+			return;
+		}
+
+		visitNum[q.front().first] = true;
+		if (q.front().first - 1 > -1 && q.front().first - 1 < 100001) {
+			if (!visitNum[q.front().first - 1]) q.push(make_pair(q.front().first - 1, q.front().second + 1));
+		}
+		if (q.front().first + 1 > -1 && q.front().first + 1 < 100001) {
+			if (!visitNum[q.front().first + 1]) q.push(make_pair(q.front().first + 1, q.front().second + 1));
+		}
+		if (q.front().first * 2 > -1 && q.front().first * 2 < 100001) {
+			if (!visitNum[q.front().first * 2]) q.push(make_pair(q.front().first * 2, q.front().second + 1));
+		}
+		q.pop();
+	}
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	bfs(n, k);
+
+	return 0;
+}
