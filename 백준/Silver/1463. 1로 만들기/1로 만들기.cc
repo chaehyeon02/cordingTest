f410d41d9a9f223aec6801eb6f60fb26c4143c5a
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-01-29 18:53:09 +0900

    Update 1로 만들기.cc

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -9,7 +9,6 @@ int main() {
 	cin >> n;
 
 	for (int i = 4; i <= n; i++) {
-		//dp.push = dp[i - 1] + 1;
 		dp.push_back(dp[i - 1] + 1);
 		if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
 		if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
