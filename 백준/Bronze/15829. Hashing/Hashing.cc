a228d7f4681f87f77846a3a956492c37228cf30a
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-02-06 03:29:34 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,18 +1,18 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-using namespace std;
+#include <stdio.h>
+#define r 31
+#define M 1234567891
 
-int main() {
-    int l, sum = 0;
-    string s;
-    cin >> l >> s;
+int main () {
+    int l, i;
+    char arr[51];
+    long long hash = 0, R = 1;
 
-    for (int i = 0; i < s.length(); i++) {
-        sum += (long)((s[i] - 96) * pow(31, i)) % 1234567891;
+    scanf("%d %s", &l, arr);
+
+    for (i = 0; i < l; i++) {
+        hash += ((arr[i] - 'a' + 1) * R) % M; hash %= M;
+        R *= r; R %= M;
     }
-
-    cout << sum;
-
+    printf("%lld", hash % M);
     return 0;
 }
