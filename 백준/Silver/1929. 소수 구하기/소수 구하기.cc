2234e505b4d636b7d1706455766fd054a368917f
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-02-08 01:50:00 +0900

    [Silver III] Title: 소수 구하기, Time: 36 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -2,22 +2,23 @@
 #include <cmath>
 using namespace std;
 
+int arr[1000001];
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
     int m, n;
-    cin >> m >> n;
 
-    for (int i = m; i <= n; i++) {
-        int max = sqrt(i);
-        if (max == 1 && i != 1) cout << i << "\n";
-        for (int j = 2; j <= max; j++) {
-            if (i % j == 0) break;
-            if (j == max) cout << i << "\n";
-        }
+    cin >> m >> n;
+    for (int i = 1; i <= n; i++) arr[i] = i;
+    
+    for (int i = 2; i <= n; i++) {
+        if (arr[i] == 0) continue;
+        for (int j = 2; i * j <= n; j++) arr[i * j] = 1;
     }
+    for (int i = m; i <= n; i++)
+        if (arr[i] != 1) cout << i << "\n";
 
     return 0;
 }
