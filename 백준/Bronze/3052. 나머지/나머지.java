86ae381c4d380615cbedb2886bc319c63e57326b
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-03-09 02:12:26 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,22 +1,16 @@
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		int n, result = 0;
-		int[] cnts = new int[42];
-		Arrays.fill(cnts, 0);
+		HashSet<Integer> h = new HashSet<>();
 
 		for(int i = 0; i < 10; i++) {
-			n = in.nextInt();
-			cnts[n % 42]++;
+			h.add(in.nextInt() % 42);
 		}
 
-		for(int i = 0; i < 42; i++) {
-			if(cnts[i] > 0) result++;
-		}
-		System.out.print(result);
+		System.out.println(h.size());
 	}
 }
