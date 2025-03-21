896e0311742bee3260b055d7e7e4c0d17ef6ec3d
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2025-03-22 02:20:40 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int a = in.nextInt();
-		int b = in.nextInt();
-		int c = in.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-		System.out.println((a + b) % c);
-		System.out.println(((a % c) + (b % c)) % c);
-		System.out.println((a * b) % c);
-		System.out.println(((a % c) * (b % c)) % c);
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
     }
 }
